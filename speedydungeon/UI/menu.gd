98fab9e32e59e7de5a6ace2e3718bc588af5e663
Shapyio <sagievg@gmail.com>
2025-02-01 04:29:58 -0500

    Added main menu. All buttons work and can go back. Added global script to manage inter-scenes actions. Global script will ultimately hold settings and player stats between rooms. TODO: Login!!!

--- speedydungeon/UI/menu.gd
+++ speedydungeon/UI/menu.gd
@@ -1,8 +1,12 @@
 extends Control
 
-
+# Load next level
+var nextScene = preload("res://level.tscn")
+	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$MarginContainer/VBoxContainer/Play.grab_focus()
+	
 	pass # Replace with function body.
 
 
@@ -12,19 +16,25 @@ func _process(delta: float) -> void:
 
 # Start button
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://level.tscn") # <-- Change the scene to load here
+	get_tree().change_scene_to_packed(nextScene) # <-- Change the scene to load here
 	pass # Replace with function body.
 
 # Settings
 func _on_settings_pressed() -> void:
+	Global.savePrevScene(get_tree().current_scene.scene_file_path)
+	get_tree().change_scene_to_file("res://UI/settings.tscn") # Load settings scene
 	pass # Replace with function body.
 
 # How to Play Button
 func _on_instructions_pressed() -> void:
+	Global.savePrevScene(get_tree().current_scene.scene_file_path)
+	get_tree().change_scene_to_file("res://UI/instructions.tscn") # Load settings scene
 	pass # Replace with function body.
 
 # Login Button
 func _on_login_pressed() -> void:
+	Global.savePrevScene(get_tree().current_scene.scene_file_path)
+	get_tree().change_scene_to_file("res://UI/login.tscn") # Load settings scene
 	pass # Replace with function body.
 
 # Exit
